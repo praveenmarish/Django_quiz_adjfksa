a6856e5f94bd61519d6dca7aad09c263cb3e2ad1
praveenmarish <praveenmarish0059@gmail.com>
2021-06-04 14:08:42 +0530

    .

--- django_quiz/urls.py
+++ django_quiz/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django_quiz.settings import STATIC_ROOT
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -22,5 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authentication.urls')),
-    path('quiz/', include('quiz.urls')),
+    path('quiz/', include('quiz.urls'))
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
